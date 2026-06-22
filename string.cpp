ce92097aef4040e7e824233169ad1d764077f93b
MAKINEEDIDEVIJAGADEESWARI <makineedidevijagadeeswari815@gmail.com>
2026-06-22 12:39:46 +0530

    Added string program

---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cout<<"enter your name:";
+    cin>>s;
+    cout<<"MY NAME IS:"<<s<<"\n";
+    int len=s.size();
+    cout<<"s[len-1] is:"<<s[len-1]<<"\n";
+    cout<<"word length:"<<len;
+}
