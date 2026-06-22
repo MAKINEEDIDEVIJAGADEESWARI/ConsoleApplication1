b268909761266c93d97923831e9d6d33c9eb587b
MAKINEEDIDEVIJAGADEESWARI <makineedidevijagadeeswari815@gmail.com>
2026-06-22 13:03:44 +0530

    sum of two numbers

---
+++ function.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void sum(int n1,int n2)
+{
+    cout<<"addition of two numbers:"<<n1+n2;
+}
+int main()
+{
+    int n1,n2;
+    cout<<"enter n1 and n2 values:"<<"\n";
+    cin>>n1>>n2;
+    sum(n1,n2);
+}
