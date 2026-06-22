fc089e96898c5b10a9265cce2196d8e4eee5b1d9
MAKINEEDIDEVIJAGADEESWARI <makineedidevijagadeeswari815@gmail.com>
2026-06-22 11:34:25 +0530

    Added switchcase program

---
+++ switchcase.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int date;
+	cout << "enter date:";
+	cin >> date;
+	switch (date) 
+	{
+	case 1:
+		cout << "MONDAY";
+		break;
+	case 2:
+		cout << "TUESDAY";
+		break;
+	case 3:
+		cout << "WEDNESDAY";
+		break;
+	case 4:
+		cout << "THURSDAY";
+		break;
+	case 5:
+		cout << "FRIDAY";
+		break;
+	case 6:
+		cout << "SATURDAY";
+		break;
+	case 7:
+		cout << "SUNDAY";
+		break;
+	default:
+		cout << "INVALID DATE";
+		break;
+	}
+	cout << "OKAY!!";
+	return 0;
+}
