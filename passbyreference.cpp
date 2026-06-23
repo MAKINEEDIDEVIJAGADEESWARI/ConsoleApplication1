2216f087d9999cdba2381a4ebae7f7b28bff4aad
MAKINEEDIDEVIJAGADEESWARI <makineedidevijagadeeswari815@gmail.com>
2026-06-23 19:21:11 +0530

    passbyreference

---
+++ passbyreference.cpp
@@ -0,0 +1,18 @@
+//pass_by_reference
+#include<bits/stdc++.h>
+using namespace std;
+void sum(int &num)
+{
+    cout<<num<<endl;
+    num+=10;
+    cout<<num<<endl;
+    num+=20;
+    cout<<num<<endl;
+}
+int main()
+{
+    int num=10;
+    sum(num);
+    cout<<num<<endl;
+    return 0;
+}
